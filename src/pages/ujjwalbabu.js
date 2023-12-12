29f6ae7f42e498ee740c101618218c91c2a4c42d
shhashank <shashanksherry@gmail.com>
2023-12-12 20:02:00 +0530

    ujjwal babu

---
+++ src/pages/ujjwalbabu.js
@@ -0,0 +1,13 @@
+export default function Ujjwalbabu() {
+  return (
+    <div>
+      <img
+        src="https://i.natgeofe.com/n/680eed24-fa0d-460a-88fe-1951b35f5a53/proboscis-monkey_thumb_2x3.jpg"
+        height={300}
+        width={300}
+        alt=""
+      />
+      <h1 style={{ color: 'red' }}>pahchan kaun</h1>
+    </div>
+  );
+}
